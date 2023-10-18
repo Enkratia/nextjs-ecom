1ea1b2afb5d9974a262ad18b14be0377bb82c235
Enkratia <manofthetreasure@gmail.com>
2023-10-18 18:03:32 +0300

    m

--- app/(.)product/[id]/page.tsx
+++ app/(.)product/[id]/page.tsx
@@ -1,4 +1,4 @@
-"use-client";
+"use client";
 
 import React from "react";
 import { useParams, useRouter } from "next/navigation";
@@ -10,24 +10,81 @@ import { StarIcon as StarIconOutline } from "@heroicons/react/24/outline";
 import { StarIcon } from "@heroicons/react/20/solid";
 
 const Modal: React.FC = () => {
-  let [isOpen, setIsOpen] = React.useState(true);
-  const id = useParams().id;
-
   const [product, setProduct] = React.useState<Product>();
+  const [loading, setLoading] = React.useState(false);
+
+  const id = useParams().id;
+  const router = useRouter();
+
+  // router.back();
 
   React.useEffect(() => {
     async function fetchProducts() {
+      setLoading(true);
+
       const res = await fetch(`https://fakestoreapi.com/products/${id}`);
       const product: Product = await res.json();
       setProduct(product);
+
+      setLoading(false);
     }
     fetchProducts();
   }, [id]);
 
   return (
-    <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
-      <Dialog.Panel>
-        {/* <Dialog.Title>Deactivate account</Dialog.Title>
+    <div
+      style={{
+        position: "fixed",
+        width: "100vw",
+        height: "100vh",
+        backgroundColor: "white",
+        zIndex: "1000",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}>
+      {product?.image && (
+        <div className="relative w-72 h-full">
+          <ProductImage product={product} fill />
+        </div>
+      )}
+
+      <div>
+        <div>
+          <h4>{product?.title}</h4>
+          <p>${product?.price}</p>
+          <div>
+            <p>{product?.rating.rate}</p>
+            {product?.rating.rate && (
+              <div>
+                {Array.from(
+                  {
+                    length: Math.floor(product.rating.rate),
+                  },
+                  (_, i) => (
+                    <StarIcon key={i} />
+                  ),
+                )}
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <button>Add to basket</button>
+        <button>View all details</button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
+
+// <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
+//   <Dialog.Panel>
+{
+  /* <Dialog.Title>Deactivate account</Dialog.Title>
         <Dialog.Description>This will permanently deactivate your account</Dialog.Description>
 
         <p>
@@ -36,18 +93,7 @@ const Modal: React.FC = () => {
         </p>
 
         <button onClick={() => setIsOpen(false)}>Deactivate</button>
-        <button onClick={() => setIsOpen(false)}>Cancel</button> */}
-
-        <div className="flex gap-x-8 h-96">
-          {product?.image && (
-            <div className="relative w-72 h-full hidden md:inline">
-              <ProductImage product={product} fill />
-            </div>
-          )}
-        </div>
-      </Dialog.Panel>
-    </Dialog>
-  );
-};
-
-export default Modal;
+        <button onClick={() => setIsOpen(false)}>Cancel</button> */
+}
+// </Dialog.Panel>
+// </Dialog>
